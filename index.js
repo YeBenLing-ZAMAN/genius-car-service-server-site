f9e6c6b8150ebca6aae1bf4505bfe841676b3607
MD KAMRUZZAMAN <kamruzzamanmd@hotmail.com>
2022-05-04 23:49:44 +0600

    order colleetion db create and get  a post req to the db form server

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ async function run() {
     try {
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
 
         app.get('/service' , async (req, res)=> {
             const query = {};
@@ -47,6 +48,13 @@ async function run() {
             const id= req.params.id;
             const query = {_id: ObjectId(id)};
             const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        })
+        
+        // order collection API
+        app.post('/order', async(req, res)=>{
+            const order = req.body;
+            const result = orderCollection.insertOne(order);
             res.send(result);
         })
 
